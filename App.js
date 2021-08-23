d2377785d6b6cf952b99a781a12c5de60f6ca051
Pedro Henrique <pedrinhr03@gmail.com>
2021-08-23 20:49:07 -0300

    Commit Inicial de Adições
    Iniciando componente para navegação entre páginas e uso de css externo para criação de estilos

--- App.js
+++ App.js
@@ -1,21 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { StyleSheet } from 'react-native';
 
-export default function App() {
+import styles from './assets/styles/styles'
+
+function InicioTela({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.basico}>
+      <Text style={styles.textosBasicos}>Página Inicial</Text>
+      <Button
+        title="Ir para outra página"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetalhesTela({ navigation }) {
+  return (
+    <View style={styles.basico}>
+      <Text style={styles.textosBasicos}>A outra página</Text>
+      <Button
+        title="Voltar Inicio"
+        onPress={() => navigation.navigate('Inicio')}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Inicio">
+        <Stack.Screen name="Inicio" component={InicioTela} />
+        <Stack.Screen name="Detalhes" component={DetalhesTela} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+// Estilos
+// const styles = StyleSheet.create({
+//   basico: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: '#333333',
+//   },
+//   textosBasicos: {
+//     color: 'red',
+//     fontSize: 25,
+//   }
+// })
