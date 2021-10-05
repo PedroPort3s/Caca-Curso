9a73c923744fc7472b66b5992485b6df0559fdd4
Pedro Henrique <pedrinhr03@gmail.com>
2021-10-04 23:10:09 -0300

    Estruturação
    Estruturação de pastas e telas do projeto

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React,  { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -6,7 +6,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import API from './src/helpers/ConsumoApi.js';
 import styles from './assets/styles/styles';
 
-function InicioTela({ navigation }) {
+function RecomendadosTela({ navigation }) {
   return (
     <View style={styles.basico}>
       <Text style={styles.textosBasicos}>Página Inicial</Text>
@@ -18,8 +18,20 @@ function InicioTela({ navigation }) {
     </View>
   );
 }
+function FavoritosTela({ navigation }) {
+  return (
+    <View style={styles.basico}>
+      <Text style={styles.textosBasicos}>Página Inicial</Text>
+      <Button
+        title="Ir para outra página 1, pq essa é a 3"
+        onPress={() => navigation.navigate('Detalhes')}
+      />
 
-function DetalhesTela({ navigation }) {
+    </View>
+  );
+}
+
+function ConfiguracoesTela({ navigation }) {
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
@@ -28,28 +40,29 @@ function DetalhesTela({ navigation }) {
     teste();
   }, []);
 
-  const teste = async () => 
-  {
+
+
+  const teste = async () => {
     try {
-      const resposta = await API.MakeRequest('https://reactnative.dev/movies.json','GET');
+      const resposta = await API.MakeRequest('https://reactnative.dev/movies.json', 'GET');
       console.log(resposta.description);
       setData(resposta.movies);
     } catch (error) {
       console.log(error);
     }
-    finally{
+    finally {
       setLoading(false);
     }
   }
 
   return (
     <View style={styles.basico}>
-      {isLoading ? <ActivityIndicator/> : (
+      {isLoading ? <ActivityIndicator /> : (
         <FlatList style={styles.textosBasicos}
           data={data}
           keyExtractor={({ id }, index) => id}
           renderItem={({ item }) => (
-            <Text onPress={()=>navigation.navigate('Inicio')}  style={styles.textosBasicos}> Filme: {item.title} Ano: {item.releaseYear}</Text>
+            <Text onPress={() => navigation.navigate('Recomendados')} style={styles.textosBasicos}> Filme: {item.title} Ano: {item.releaseYear}</Text>
           )}
         />
       )}
@@ -63,25 +76,27 @@ const Tab = createBottomTabNavigator();
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Inicio" screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+      <Tab.Navigator initialRouteName="Recomendados" screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'Inicio') {
-              iconName = focused
-                ? 'home' : 'home-outline';
-            } else if (route.name === 'Detalhes') {
-              iconName = focused ? 'ellipsis-vertical' : 'ellipsis-vertical-outline';
-            }
-            
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: '#00FFC1',
-          tabBarInactiveTintColor: '#00418F',
-        })}
-        >
-        <Tab.Screen name="Inicio" component={InicioTela} />
-        <Tab.Screen name="Detalhes" component={DetalhesTela} />
+          if (route.name === 'Recomendados') {
+            iconName = focused ? 'md-star' : 'md-star-outline';
+          } else if (route.name === 'Configuracoes') {
+            iconName = focused ? 'settings' : 'settings-outline';
+          } else if (route.name === 'Favoritos') {
+            iconName = focused ? 'heart' : 'heart-outline';
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#000',
+        tabBarInactiveTintColor: '#000',
+      })}
+      >
+        <Tab.Screen name="Recomendados" component={RecomendadosTela} />
+        <Tab.Screen name="Configuracoes" component={ConfiguracoesTela} />
+        <Tab.Screen name="Favoritos" component={FavoritosTela} />
       </Tab.Navigator>
     </NavigationContainer>
   );
